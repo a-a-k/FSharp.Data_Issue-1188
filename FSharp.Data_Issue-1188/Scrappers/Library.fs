729a731561684fd2f566a4e02e494be5691dc696
FoggyFinder <FoggyFinder@yandex.ua>
2018-08-05 14:04:38 +0300

    part 1

--- FSharp.Data_Issue-1188/Scrappers/Library.fs
+++ FSharp.Data_Issue-1188/Scrappers/Library.fs
@@ -6,50 +6,69 @@ namespace Scrappers
    open System.Threading.Tasks
    open System
 
+   module HtmlNode = 
+       let tryGetAttributeValue attr = 
+           HtmlNode.tryGetAttribute attr
+           >> Option.map HtmlAttribute.value 
+           
    module Aptekaru =
+       let baseUrl = "https://apteka.ru/"
+       let search = sprintf "search/?PAGEN_products=%i&q=%s"
+       let cookie = CookieContainer()
+       let request url_part = 
+           let url = sprintf "%s%s" baseUrl url_part 
+           Http.RequestString(url, cookieContainer = cookie)
 
-         let baseUrl = "https://apteka.ru/"
-         let cookies = CookieContainer()
-         let request url_part = 
-           Http.RequestString
-             ( sprintf "%s%s" baseUrl url_part,
-               cookieContainer = cookies )
+       let setZone id = 
+           sprintf "_action/geoip/setBranch/%s/" id
+           |> request |> ignore
 
-         request "" |> ignore
+       let parse pages query = Task.FromResult(struct (new List<ItemPrice>(), ""))
 
-         let parse pages query = Task.FromResult(struct (new List<ItemPrice>(), ""))
+       let nodeToItemPrice el = 
+           let amount = 
+               HtmlNode.tryGetAttributeValue "data-product-quantity" el
+               |> Option.map int
+               |> Option.toNullable
 
-         let rec parse_search query step (pages: List<string>) (items: List<ItemPrice>) =
-           let doc = HtmlDocument.Parse(request (sprintf "search/?PAGEN_products=%i&q=%s" step query))
+           let id = 
+               HtmlNode.tryGetAttributeValue "data-product-id" el
+               |> Option.defaultValue "-" 
+
+           let name = 
+               HtmlNode.tryGetAttributeValue "data-product-name" el
+               |> Option.defaultValue "-no name-"
+
+           let price = 
+               HtmlNode.tryGetAttributeValue "data-product-price" el
+               |> Option.map decimal
+               |> Option.defaultValue 0M
+
+           new ItemPrice(Amount = amount, Id = id, Name = name, Price = price)
+
+       let rec parse_search query step (pages: List<string>) (items: List<ItemPrice>) =
+           let url = search step query 
+           let doc =  url |> request |> HtmlDocument.Parse
+           
            pages.Add(doc.Html().InnerText())
-           let items_list = doc.CssSelect("div.list.catalog-list article") // always empty, also as "div.items.catalog-items > article", "article", etc.
-           items.AddRange
-             ( items_list
-                 |> Seq.map(fun el -> new ItemPrice (
-                      Amount = (match el.TryGetAttribute("data-product-quantity") with 
-                                | None -> 0 |> Nullable<int> 
-                                | Some attr -> attr.Value() |> int |> Nullable<int>), 
-                      Id = (match el.TryGetAttribute("data-product-id") with 
-                            | None -> "-" 
-                            | Some attr -> attr.Value()), 
-                      Name = (match el.TryGetAttribute("data-product-name") with 
-                              | None -> "-no name-" 
-                              | Some attr -> attr.Value()), 
-                      Price = (match el.TryGetAttribute("data-product-price") with 
-                               | None -> 0 |> decimal 
-                               | Some attr -> attr.Value() |> decimal) )))
 
-           match doc.CssSelect("ul.pagin_items li.arrow_next a").IsEmpty with // always empty
-           | false -> parse_search query (step + 1) pages items
-           | true -> doc.CssSelect("div.region-select__name").Head.InnerText() // here works ok
+           let items_list = 
+               doc.CssSelect("div.list.catalog-list article") // always empty, also as "div.items.catalog-items > article", "article", etc.
+               |> Seq.map nodeToItemPrice         
+           
+           items_list |> items.AddRange 
 
-         let search_and_parse zoneId query =
-           request (sprintf "_action/geoip/setBranch/%s/" zoneId) |> ignore
-           let mutable items = new List<ItemPrice>()
-           let mutable pages = new List<string>()
+           match doc.CssSelect("ul.pagin_items li.arrow_next a") with // always empty
+           | [] -> doc.CssSelect("div.region-select__name").Head.InnerText() // here works ok
+           | _ -> parse_search query (step + 1) pages items
+
+       let search_and_parse zoneId query =
+           zoneId |> setZone
+           let items = new List<ItemPrice>()
+           let pages = new List<string>()
            Task.FromResult(struct (pages, items, parse_search query 1 pages items))
 
-     type AptekaruAdapter() =
-         interface IAdapter with
-             member this.Parse(pages, query) = Aptekaru.parse pages query
-             member this.SearchAndParse(zoneId, query) = Aptekaru.search_and_parse zoneId query       
+   type AptekaruAdapter() =
+       interface IAdapter with
+           member this.Parse(pages, query) = Aptekaru.parse pages query
+           member this.SearchAndParse(zoneId, query) = Aptekaru.search_and_parse zoneId query       
